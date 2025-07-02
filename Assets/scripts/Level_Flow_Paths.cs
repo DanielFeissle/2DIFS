8ba68f2fe829229cfc50d7cc62fd7365c0dcedf3
Daniel Feissle <dfeissle@msn.com>
2025-07-01 19:40:18 -0500

    Adjust to the web loader
    Also moved around file directories to avoid loading all at once

--- Assets/scripts/Level_Flow_Paths.cs
+++ Assets/scripts/Level_Flow_Paths.cs
@@ -17,25 +17,51 @@ public class Level_Flow_Paths : MonoBehaviour
 
 public string checkLevels(int scene,int world)
     {
-        Debug.Log("HHHHHHHHHHHHHHHHHHHHHHHIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
-        StreamReader reader = new StreamReader("Assets/Resources/scenes/flow.txt");
-        int count = 0;
-        string txt = reader.ReadToEnd();
         string totScenesPerWorld = "";
-        foreach (string tta in txt.Split('\n'))
+        try
         {
-            if (world==count )
+            StreamReader reader = new StreamReader("Assets/Resources/scenes/flow.txt");
+            int count = 0;
+            string txt = reader.ReadToEnd();
+            
+            foreach (string tta in txt.Split('\n'))
             {
-                //we are on the correct scene setup
-                string[] master_data = tta.Split(';');
-                //master data 0 is the background (if used)
-                //master data 1 is the scenes per level
-                string[] ind_scn = master_data[1].Split(',');
-                totScenesPerWorld = world + "x" + ind_scn.Length;
-            }
+                if (world == count)
+                {
+                    //we are on the correct scene setup
+                    string[] master_data = tta.Split(';');
+                    //master data 0 is the background (if used)
+                    //master data 1 is the scenes per level
+                    string[] ind_scn = master_data[1].Split(',');
+                    totScenesPerWorld = world + "x" + ind_scn.Length;
+                }
 
-            count++;
+                count++;
+            }
         }
+        catch
+        {
+            int count = 0;
+            string txt = @"NONE;1x1,1x2,1x3,1x4
+NONE;2x1,2x2,2x3,2x4
+NONE;3x1,3x2,3x3,3x4
+NONE;4x1,4x2,4x3,4x4";
+            foreach (string tta in txt.Split('\n'))
+            {
+                if (world == count)
+                {
+                    //we are on the correct scene setup
+                    string[] master_data = tta.Split(';');
+                    //master data 0 is the background (if used)
+                    //master data 1 is the scenes per level
+                    string[] ind_scn = master_data[1].Split(',');
+                    totScenesPerWorld = world + "x" + ind_scn.Length;
+                }
+
+                count++;
+            }
+        }
+
         return totScenesPerWorld;
     }
 }
--- Assets/scripts/first_time_start.cs
+++ Assets/scripts/first_time_start.cs
@@ -7,6 +7,20 @@ public class first_time_start : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //7-1-2025
+
+#if UNITY_EDITOR
+        Debug.Log("Running in the Unity Editor");
+#elif UNITY_STANDALONE
+    Debug.Log("Running as a standalone EXE");
+#elif UNITY_WEBGL
+    Debug.Log("Running in a web browser (WebGL)");
+    Debug.unityLogger.logEnabled = false;
+#else
+    Debug.Log("Unknown platform");
+#endif
+
+
         Time.timeScale = 1;
         //11-17-2022 this script gets called once per game load
         if (!GameObject.Find("sela"))
